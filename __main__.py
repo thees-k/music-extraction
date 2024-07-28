5c8373c43c8cf32896a286a5c748e75b2395f724
Thees K <ts.custer@t-online.de>
2024-07-28 15:14:21 +0200

    Improved and tested version

--- __main__.py
+++ __main__.py
@@ -26,14 +26,16 @@ def get_speech_segment(segment_path, recognizer):
 
 def analyze_audio_segments(wav_path, segment_length_sec=20):
     recognizer = sr.Recognizer()
+
     segments = []
-    music_segments = []
+    music_start = None
 
     total_length = AudioSegment.from_wav(wav_path).duration_seconds
     total_length_display = seconds_to_min_sec(int(total_length))
     start_time = 0
 
-    is_music = True
+    is_music_running = False
+    music_begin = 0
 
     while start_time < total_length:
         end_time = start_time + segment_length_sec
@@ -47,32 +49,25 @@ def analyze_audio_segments(wav_path, segment_length_sec=20):
 
         speech_segment = get_speech_segment(segment_path, recognizer)
         if speech_segment:
-            if is_music:
-                is_music = False
+            if is_music_running:
+                segments.append((music_begin, end_time, speech_segment))
+                is_music_running = False
                 print(f"-> Music stop between {seconds_to_min_sec(start_time)} and {seconds_to_min_sec(end_time)} :"
                       f" \"{speech_segment}\"")
         else:
-            segments.append((start_time, end_time))
-            if not is_music:
-                is_music = True
+            if not is_music_running:
+                music_begin = max(start_time - segment_length_sec, 0)
+                is_music_running = True
                 print(f"-> Music START between {seconds_to_min_sec(start_time)} and {seconds_to_min_sec(end_time)}")
 
         os.remove(segment_path)
 
         start_time = end_time
 
-    if not segments:
-        return music_segments
+    if is_music_running:
+        segments.append((music_begin, total_length, ""))
 
-    music_start = segments[0][0]
-    for i in range(1, len(segments)):
-        if segments[i][0] != segments[i - 1][1]:
-            music_end = segments[i - 1][1]
-            music_segments.append((music_start, music_end))
-            music_start = segments[i][0]
-    music_segments.append((music_start, segments[-1][1]))
-
-    return music_segments
+    return segments
 
 
 def seconds_to_min_sec(seconds):
@@ -83,11 +78,13 @@ def seconds_to_min_sec(seconds):
 
 def print_music_segments(segments):
     print("\nMusic segments:")
-    for start, end in segments:
+    for no, segment in enumerate(segments, start=1):
+        start, end, speech = segment
         start = int(start)
         end = int(end)
         length = end - start
-        print(f"Music from {seconds_to_min_sec(start)} to {seconds_to_min_sec(end)} -> {seconds_to_min_sec(length)}")
+        print(f"{no}) {seconds_to_min_sec(start)} to {seconds_to_min_sec(end)} -> {seconds_to_min_sec(length)} ({speech})")
+        print()
 
 
 def main():
@@ -96,10 +93,11 @@ def main():
     convert_mp3_to_wav(mp3_path, wav_path)
 
     segments = analyze_audio_segments(wav_path)
-    print_music_segments(segments)
 
     os.remove(wav_path)
     print(f"Removed temporary file {wav_path}")
+
+    print_music_segments(segments)
 
 
 if __name__ == "__main__":
