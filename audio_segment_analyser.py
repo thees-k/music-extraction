fbdad58b484746017c9f2e1e2617940c0ff309a8
Thees K <ts.custer@t-online.de>
2024-08-05 15:47:02 +0200

    My changes of audio_segment_analyser.py to get no errors anymore

--- audio_segment_analyser.py
+++ audio_segment_analyser.py
@@ -6,7 +6,7 @@ import os
 class AudioSegmentAnalyser:
     def __init__(self):
         self._language = "de-DE"
-        self._model_path = "/path/to/de-de"  # Update this path to where you have your German models
+        self._model_path = "/home/thees/.local/lib/python3.10/site-packages/pocketsphinx/model/en-us/"
 
     @property
     def language(self):
@@ -27,20 +27,21 @@ class AudioSegmentAnalyser:
             str: The recognized text from the audio segment.
         """
         config = {
-            'verbose': False,
+            'verbose': True,
             'audio_file': str(segment_path),
             'buffer_size': 2048,
             'no_search': False,
             'full_utt': False,
-            'hmm': os.path.join(self._model_path, 'acoustic-model'),  # Update to the folder containing mdef
-            'lm': os.path.join(self._model_path, 'language-model.lm.bin'),  # Update to the language model file
-            'dict': os.path.join(self._model_path, 'phonetic.dict')  # Update to the phonetic dictionary
+            'hmm': os.path.join(self._model_path, 'en-us'),  # Update to the folder containing mdef
+            'lm': os.path.join(self._model_path, 'en-us.lm.bin'),  # Update to the language model file
+            'dict': os.path.join(self._model_path, 'cmudict-en-us.dict')  # Update to the phonetic dictionary
         }
 
         audio = AudioFile(**config)
         recognized_text = []
 
         for phrase in audio:
+            print(f"phrase {phrase}")
             recognized_text.append(str(phrase))
 
-        return " ".join(recognized_text).strip()
+        return " ".join(recognized_text)
