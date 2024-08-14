750a6b89dc6e7798d273c0e983298039d1e6068f
Thees K <ts.custer@t-online.de>
2024-08-14 14:26:06 +0200

    Improved seconds_formatter.py

--- seconds_formatter.py
+++ seconds_formatter.py
@@ -1,5 +1,10 @@
 
 def seconds_to_min_sec(seconds):
-    minutes = seconds // 60
-    remaining_seconds = seconds % 60
-    return f"{minutes}:{remaining_seconds:02}"
+    full_seconds = int(seconds)
+    millis = seconds - full_seconds
+    minutes = full_seconds // 60
+    remaining_seconds = full_seconds % 60
+    if millis:
+        return f"{minutes}:{remaining_seconds:02}.{int(millis * 1000)}"
+    else:
+        return f"{minutes}:{remaining_seconds:02}"
